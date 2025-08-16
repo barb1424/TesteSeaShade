847650fa40e2f5294c8fb771c8e3826b84f9af15
brunoss21 <bruno_silva1420@hotmail.com>
2025-08-16 18:34:17 -0300

    Primeiro commit 16/08/25

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>SEASHADE</h1>
+        <h2>Leveza e eficiência para o seu negócio à beira-mar.</h2>
+        <p class="font-sans ... text-blue-600 dark:text-sky-400">The quick brown fox ...</p>
+        <p class="font-serif ...">The quick brown fox ...</p>
+        <p class="font-mono ...">The quick brown fox ...</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+/* import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+*/
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import './index.css'
+import App from "./App.jsx";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);

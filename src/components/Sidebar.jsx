a3880e9621582a75fb5040a34b9b4a2be97e14d1
babi <arandomwalrus1424@hotmail.com>
2025-08-29 06:48:38 -0300

    Ajustes finos menu lateral

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
     const[expanded, setExpanded] = useState(true);
 
     return (
-        <aside className={`transition-all duration-300 h-screen flex ${expanded? "w-2/3 md:w-1/3 lg:w-1/5" : "w-16"}`}>
-        <header className="text-slate-600 w-full h-full flex flex-col bg-slate-100 shadow-sm font-medium border-r border-azul-100">
-            <div className="flex items-center text-slate-600 border-b border-azul-100 mb-2">
+        <aside className={`transition-all duration-300 border-b border-azul-100 border-r h-screen flex bg-slate-100 shadow-sm flex-col ${expanded? "w-2/3 md:w-1/3 lg:w-1/5" : "w-16"}`}>
+        <header className="text-slate-600 w-full flex flex-col  font-medium  h-full">
+            <div className="flex items-center text-slate-600 mb-2">
 
                 <div className={`flex-1 overflow-hidden transition-all duration-300${expanded? "p-4 pl-6 flex-grow" : "w-0"} `}>
                     <img src={logo} alt="Logo do Seashade" className="w-28"/>
@@ -19,21 +19,22 @@ const Sidebar = () => {
                 </button>
 
             </div>
-            <nav className="h-full text-lg flex flex-col px-3 gap-2">
-                <ul className="flex flex-col justify-around flex-3 border-b border-azul-100 pb-2">
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><LayoutPanelLeft/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Painel</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><NotebookPen/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Pedidos</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><BookOpenText/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Cardápio</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><PackageOpen/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Estoque</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><ChartSpline/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Relatórios</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><ChefHat/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Modo Produção</div></li>
+            <nav className="text-lg flex flex-col px-3 gap-2">
+                <ul className="flex flex-col flex-3 border-b border-azul-100 pb-2">
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><LayoutPanelLeft/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Painel</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><NotebookPen/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Pedidos</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><BookOpenText/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Cardápio</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><PackageOpen/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Estoque</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><ChartSpline/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Relatórios</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><ChefHat/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Modo Produção</div></li>
                 </ul>
                 <ul className="flex flex-col flex-1 justify-around">
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><Info/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Ajuda</div></li>
-                    <li className="cursor-pointer h-full flex px-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><Settings /></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all duration-300 w-0"}`}>Configurações</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><Info/></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all w-0"}`}>Ajuda</div></li>
+                    <li className="cursor-pointer h-full flex p-2 items-center hover:bg-azul-100 hover:text-azul-600 rounded-lg transition-all duration-300 truncate"><div className="min-w-10"><Settings /></div><div className={`${expanded? "flex-grow" : "overflow-hidden transition-all duration-300 w-0"}`}>Configurações</div></li>
                 </ul>
         </nav>
-       <div className="relative px-4 cursor-pointer hover:bg-slate-200 transition-all duration-300 mt-2 flex border-t border-azul-100 text-slate-600 items-center justify-center py-4">
+    </header>
+       <div className="relative px-4 cursor-pointer hover:bg-slate-200 transition-all duration-300 flex border-t border-azul-100 text-slate-600 items-center justify-center py-4">
             <div className={`flex items-center overflow-hidden transition-all duration-300 ${expanded ? "flex-1 gap-3" : "w-0"}`}>
                 <div className="rounded aspect-square bg-azul-200 flex justify-center items-center font-extrabold text-azul-600 w-9 md:w-13">
                     <User size="auto"/>
@@ -47,7 +48,6 @@ const Sidebar = () => {
              <EllipsisVertical size={22}/>
             </button>
         </div>
-    </header>
 </aside>
     )
 }
